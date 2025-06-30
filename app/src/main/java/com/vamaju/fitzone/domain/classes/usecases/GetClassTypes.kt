5bbb8c9e2fee841d56178fb62da2ddd7b5f706fe
Juan Collantes <jcollantes@skgtecnologia.com>
2025-06-30 11:48:16 -0500

    feat: Base_Domain_Layer = create posibles usescases

---
+++ app/src/main/java/com/vamaju/fitzone/domain/classes/usecases/GetClassTypes.kt
@@ -0,0 +1,7 @@
+package com.vamaju.fitzone.domain.classes.usecases
+
+/**
+ * @author Juan Camilo Collantes Tovar on 30/06/2025
+ * **/
+class GetClassTypes {
+}
---
+++ app/src/main/java/com/vamaju/fitzone/domain/locations/usecases/GetLocations.kt
@@ -0,0 +1,7 @@
+package com.vamaju.fitzone.domain.locations.usecases
+
+/**
+ * @author Juan Camilo Collantes Tovar on 30/06/2025
+ * **/
+class GetLocations {
+}
